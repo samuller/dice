308e0fe78c667eeb5eb78267a33634c5b2013d9d
Simon Muller <samullers@gmail.com>
2014-11-25 06:55:44 +0200

    License

--- dice.py
+++ dice.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
-"""A command-line tool for simulating various dice throw situations."""
+"""A command-line tool for simulating various dice throw situations.
+
+Copyright (C) 2014  Simon Muller
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+"""
 import sys
 if sys.version_info.major != 2:
     raise Exception("Only Python version 2 supported, not %s." %
